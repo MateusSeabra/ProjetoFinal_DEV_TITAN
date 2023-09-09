ec335f143c49beedba916bcdc5a2c8e723a6f47f
Fabio.Jose <fabiojas@ufba.br>
2023-09-09 12:50:38 -0300

    HTML dinâmico para notícias

--- Front-end/Página de Notícias/newsFunction.js
+++ Front-end/Página de Notícias/newsFunction.js
@@ -1,44 +1,44 @@
-fetch("http://localhost:3000/news")
-    .then((res)=>{
-        return res.json()
-    })
-    .then((data)=>{
-        console.log(data)
-        atualizarDivsDeNoticias(data)
-    })
-    .catch((error)=> {
-        console.log(error)
-    })
 
-    function atualizarDivsDeNoticias(data) {
-        for (const categoria in data.news) {
-          data.news[categoria].forEach(item => {
-            const divNoticia = document.getElementById(item.id);
+async function loadAndGenerateNewsHTML() {
+    try {
+      const response = await fetch('http://localhost:3000/news'); // Caminho para o seu arquivo JSON
+      if (!response.ok) {
+        throw new Error('Erro ao carregar o arquivo JSON');
+      }
+      const newsData = await response.json();
+  
+      // Suponha que suas categorias estejam definidas em um array
+      const categories = ['sports', 'tech', 'arts', 'others'];
+  
+      categories.forEach((category) => {
+        const categoryNews = newsData.news[category];
+        generateNewsHTML(category, categoryNews);
+      });
+    } catch (error) {
+      console.error('Erro ao carregar dados das notícias:', error);
+    }
+  }
+  
+  // Chame a função para carregar e gerar o HTML das notícias
+  loadAndGenerateNewsHTML();
+      function generateNewsHTML(category, news) {
+        const newsContainer = document.getElementById("news-container");
+        const categoryClass = category.toLowerCase();
       
-            if (divNoticia) {
-              // Atualize as informações na div de notícia
-              const imagem = divNoticia.querySelector('img');
-              const titulo = divNoticia.querySelector('.news-title');
-              const autor = divNoticia.querySelector('.news-date');
-      
-              if (imagem) {
-                imagem.src = item.image;
-                imagem.alt = item.title;
-              }
-      
-              if (titulo) {
-                titulo.textContent = item.title;
-              }
-      
-              if (autor) {
-                autor.textContent = item.author;
-              }
-            }
-          });
-        }
+        news.forEach((item) => {
+          const newsHTML = `
+            <div class="col-md-4 project-box ${categoryClass}" id="${item.id}">
+              <img src="${item.image}" class="img-fluid" alt="${item.title}">
+              <p class="news-title">${item.title}</p>
+              <p class="news-date">Autor: ${item.author}</p>
+            </div>
+          `;
+          newsContainer.innerHTML += newsHTML;
+        });
       }
       
-      // Chama a função para atualizar as divs de notícias quando a página carrega
-      window.addEventListener('load', function () {
-        atualizarDivsDeNoticias(dadosDoBancoDeDados);
-      });
+      // Gerar o HTML para cada categoria
+      generateNewsHTML("sports", newsData.news.sports);
+      generateNewsHTML("tech", newsData.news.tech);
+      generateNewsHTML("arts", newsData.news.arts);
+      generateNewsHTML("others", newsData.news.others);
